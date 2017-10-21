ea5577465ce5a62bdaff789f8a2a7057e442771f
kphonasa <pwnasa@gmail.com>
2017-10-21 00:25:52 -0700

    added functionality to up/download buttons and sliders

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(function() {
+    //creating uploaded img on canvas from
     var imgInput = document.getElementById('imgInput');
     imgInput.addEventListener('change', handleImage, false);
     var canvas = document.getElementById('canvas');
@@ -18,6 +19,7 @@ $(function() {
         reader.readAsDataURL(e.target.files[0]);
     }
 
+    //filter buttons functions
     var $brightness = $('#brightnessbtn');
     var $noise = $('#noisebtn');
     var $sepia = $('#sepiabtn');
@@ -239,59 +241,184 @@ $(function() {
         });
     });
 
+    //slider functions
 
-    /*$('input[type=range]').change(applyFilters());
+    var $br = $('#br');
+    var $vb = $('#vb');
+    var $h = $('#h');
+    var $gam = $('#gam');
+    var $cli = $('#cli');
+    var $ct = $('#ct');
+    var $saturate = $('#saturate');
+    var $ex = $('#ex');
+    var $sep = $('#sep');
+    var $n = $('#n');
 
-    function applyFilters(img) {
-        var br = parseInt($('#br').val());
-        var vb = parseInt($('#vb').val());
-        var h = parseInt($('#h').val());
-        var gam = parseInt($('#gam').val());
-        var cli = parseInt($('#cli').val());
-        var ct = parseInt($('#ct').val());
-        var saturate = parseInt($('#saturate').val());
-        var ex = parseInt($('#ex').val());
-        var sep = parseInt($('#sep').val());
-        var n = parseInt($('#n').val());
-
-        Caman('#canvas', function () {
-            this.revert(false);
-            /*this.brightness(br);
-            this.vibrance(vb);
-            this.hue(h);
-            this.gamma(gam);
-            this.clip(cli);
-            this.contrast(ct);
-            this.saturation(saturate);
-            this.exposure(ex);
-            this.sepia(sep);
-            this.noise(n);
+    $br.on('change', function(img) {
+        var brval = parseInt($('#br').val());
+        brFunction();
+        Caman('#canvas', img, function() {
+            this.brightness(brval);
             this.render();
-            this.brightness(br).vibrance(vb).hue(h).gamma(gam).clip(cli).contrast(ct).saturation(saturate).exposure(ex).sepia(sep).noise(n).render();
         });
-    }*/
+    });
 
+    function brFunction() {
+        var x = document.getElementById("br").value;
+        document.getElementById("br-label").innerHTML = "Brightness:" + x;
+    }
+
+    $vb.on('change', function(img) {
+        var vbval = parseInt($('#vb').val());
+        vbFunction();
+        Caman('#canvas', img, function() {
+            this.vibrance(vbval);
+            this.render();
+        });
+    });
+
+    function vbFunction() {
+        var x = document.getElementById("vb").value;
+        document.getElementById("vb-label").innerHTML = "Vibrance:" + x;
+    }
+
+    $h.on('change', function(img) {
+        var hval = parseInt($('#h').val());
+        hFunction();
+        Caman('#canvas', img, function() {
+            this.hue(hval);
+            this.render();
+        });
+    });
+
+    function hFunction() {
+        var x = document.getElementById("h").value;
+        document.getElementById("h-label").innerHTML = "Hue:" + x;
+    }
+
+    $gam.on('change', function(img) {
+        var gamval = parseInt($('#gam').val());
+        gamFunction();
+        Caman('#canvas', img, function() {
+            this.gamma(gamval);
+            this.render();
+        });
+    });
+
+    function gamFunction() {
+        var x = document.getElementById("gam").value;
+        document.getElementById("gam-label").innerHTML = "Gamma:" + x;
+    }
+
+    $cli.on('change', function(img) {
+        var clival = parseInt($('#cli').val());
+        cliFunction();
+        Caman('#canvas', img, function() {
+            this.clip(clival);
+            this.render();
+        });
+    });
+
+    function cliFunction() {
+        var x = document.getElementById("cli").value;
+        document.getElementById("cli-label").innerHTML = "Clip:" + x;
+    }
+
+    $ct.on('change', function(img) {
+        var ctval = parseInt($('#ct').val());
+        ctFunction();
+        Caman('#canvas', img, function() {
+            this.contrast(ctval);
+            this.render();
+        });
+    });
+
+    function ctFunction() {
+        var x = document.getElementById("ct").value;
+        document.getElementById("ct-label").innerHTML = "Contrast:" + x;
+    }
+
+    $saturate.on('change', function(img) {
+        var saturateval = parseInt($('#saturate').val());
+        saturateFunction();
+        Caman('#canvas', img, function() {
+            this.saturation(saturateval);
+            this.render();
+        });
+    });
+
+    function saturateFunction() {
+        var x = document.getElementById("saturate").value;
+        document.getElementById("saturate-label").innerHTML = "Saturation:" + x;
+    }
+
+    $ex.on('change', function(img) {
+        var exval = parseInt($('#ex').val());
+        exFunction();
+        Caman('#canvas', img, function() {
+            this.exposure(exval);
+            this.render();
+        });
+    });
+
+    function exFunction() {
+        var x = document.getElementById("ex").value;
+        document.getElementById("ex-label").innerHTML = "Exposure:" + x;
+    }
+
+    $sep.on('change', function(img) {
+        var sepval = parseInt($('#sep').val());
+        sepFunction();
+        Caman('#canvas', img, function() {
+            this.sepia(sepval);
+            this.render();
+        });
+    });
+
+    function sepFunction() {
+        var x = document.getElementById("sep").value;
+        document.getElementById("sep-label").innerHTML = "Sepia:" + x;
+    }
+
+    $n.on('change', function(img) {
+        var nval = parseInt($('#n').val());
+        nFunction();
+        Caman('#canvas', img, function() {
+            this.noise(nval);
+            this.render();
+        });
+    });
+
+    function nFunction() {
+        var x = document.getElementById("n").value;
+        document.getElementById("n-label").innerHTML = "Noise:" + x;
+    }
+
+    //Reset and save buttons
 
     $("#resetbtn").click(function(img) {
         $('input[type=range]').val(0);
+        brFunction();
+        vbFunction();
+        hFunction();
+        gamFunction();
+        cliFunction();
+        ctFunction();
+        saturateFunction();
+        exFunction();
+        sepFunction();
+        nFunction();
         Caman('#canvas', img, function () {
             this.revert();
             this.render();
         });
     });
 
-    /*$('#savebtn').click(function(img){
-        Caman('#canvas', img, function () {
-            this.render(function () {
-                this.save('jpg');
-            });
-        });
-
-    });
-
     function download() {
         var dt = canvas.toDataURL('image/jpeg');
         this.href = dt;
+        var fileName = prompt("What would you like to name this photo?", "Harry Potter");
+        this.download = fileName + ".jpg";
     }
-    downloadLnk.addEventListener('click', download, false);*/
+    downloadLnk.addEventListener('click', download, false);
 });
